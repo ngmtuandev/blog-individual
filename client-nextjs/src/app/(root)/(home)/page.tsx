d3d55cc7ff243a0364a2c77755cf9ce265e92b51
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-12-15 18:35:16 +0700

    'fix'

--- client-nextjs/src/app/(root)/(home)/page.tsx
+++ client-nextjs/src/app/(root)/(home)/page.tsx
@@ -16,9 +16,9 @@ const HomePage = () => {
   return (
     <div className="text-gray-800 flex gap-5 flex-wrap items-center justify-center mt-8 ">
       {posts &&
-        posts?.map((item) => {
+        posts?.map((item, index) => {
           return (
-            <div className="w-[30%]">
+            <div key={index} className="w-[30%]">
               <Post></Post>
             </div>
           );
