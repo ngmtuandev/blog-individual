f5cc496914ef8f88471991c46adc398a13859595
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-12-15 18:39:25 +0700

    'fix-local'

--- client-nextjs/src/app/store/auth.store.ts
+++ client-nextjs/src/app/store/auth.store.ts
@@ -3,21 +3,24 @@ import axiosClient from "../libs/axios-client";
 
 export const useAuthStore = create((set) => ({
   dataUser: null,
-  isLogin: !!localStorage.getItem("user-login-blog"),
+  isLogin:
+    typeof window !== "undefined" && !!localStorage.getItem("user-login-blog"),
   setIsLogin: (isLogin: boolean) => set({ isLogin }),
   setDataUser: async () => {
-    const token = localStorage.getItem("user-login-blog");
-    if (token) {
-      try {
-        const user = await axiosClient(`/auth/me`);
-        if (user) {
-          set({ dataUser: user?.data?.data });
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("user-login-blog");
+      if (token) {
+        try {
+          const user = await axiosClient(`/auth/me`);
+          if (user) {
+            set({ dataUser: user?.data?.data });
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
+      } else {
+        set({ dataUser: undefined });
       }
-    } else {
-      set({ dataUser: undefined });
     }
   },
 }));
