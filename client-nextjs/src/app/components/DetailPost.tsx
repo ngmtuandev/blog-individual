26e16cfd5850c9deb3bf4403da0d419bc543bed0
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-12-16 14:29:28 +0700

    'comment-post'

--- client-nextjs/src/app/components/DetailPost.tsx
+++ client-nextjs/src/app/components/DetailPost.tsx
@@ -2,22 +2,24 @@
 import React from "react";
 import axiosClient from "../libs/axios-client";
 import { useRouter } from "next/navigation";
-
+import { useDisclosure } from "@nextui-org/react";
+import ModelComment from "./ModelComment";
 const DetailPost = ({ post }: { post: TPost }) => {
   const router = useRouter();
-  console.log("post >>>>", post);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const handleLikePost = async (slug: string) => {
     const res = await axiosClient.put(`/posts/${slug}/like`);
     if (res) {
       router.refresh();
     }
   };
+  console.log(post);
   return (
     <div className="flex flex-col justify-center items-center">
       <div>
         <img
           src={post?.thumb}
-          className="lg:w-full lg:h-full object-cover my-4"
+          className="lg:w-full lg:h-[400px] object-cover my-4"
           alt="banner-post"
         ></img>
       </div>
@@ -27,7 +29,6 @@ const DetailPost = ({ post }: { post: TPost }) => {
             {post?.title}
           </h3>
           <div dangerouslySetInnerHTML={{ __html: post?.content }} />
-          {/* <span>{post?.content}</span> */}
         </div>
         <div className="py-5 border-t-1 border-b-1 my-6">
           <div className="flex items-center gap-6">
@@ -37,11 +38,19 @@ const DetailPost = ({ post }: { post: TPost }) => {
             >
               {`Like (${post?.likes?.length})`}
             </span>
-            <span>Like</span>
+            <span className="cursor-pointer" onClick={onOpen}>
+              Comment
+            </span>
             <span>Like</span>
           </div>
         </div>
       </div>
+      <ModelComment
+        slug={post?.slug}
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onOpenChange={onOpenChange}
+      ></ModelComment>
     </div>
   );
 };
--- client-nextjs/src/app/components/Post.tsx
+++ client-nextjs/src/app/components/Post.tsx
@@ -4,7 +4,6 @@ import { Skeleton } from "@nextui-org/react";
 import Link from "next/link";
 
 const Post = ({ post }: { post: TPost }) => {
-  console.log("post item ", post);
   return (
     <Link href={`/blog/${post?.slug}`}>
       <Card className="py-4 lg:min-h-full">
@@ -16,7 +15,7 @@ const Post = ({ post }: { post: TPost }) => {
         <CardBody className="overflow-visible py-2">
           <Image
             alt="Card background"
-            className="object-cover rounded-xl"
+            className="object-cover rounded-xl lg:h-[250px]"
             src={
               post?.thumb ??
               "https://images.pexels.com/photos/16756656/pexels-photo-16756656/free-photo-of-black-and-white-photo-of-a-swan-on-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
