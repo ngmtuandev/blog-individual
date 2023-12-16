772532600c9449cfea202a0c69f2f4a961837f09
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-12-17 06:13:24 +0700

    'test-framer'

---
+++ client-nextjs/src/app/components/FramerImgMe.tsx
@@ -0,0 +1,27 @@
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import profilePic from "../asset/image/me.png";
+const FramerImgMe = () => {
+  return (
+    <div className="flex h-[100px] items-center justify-center">
+      <motion.div
+        initial={{ opacity: 0.5, scale: 0.5, x: 0 }}
+        animate={{
+          x: 600,
+          y: 0,
+          scale: 2,
+        }}
+        // exit={{ opacity: 0, x: 100 }}
+        transition={{ duration: 3 }}
+        whileInView={{ opacity: 1, scale: 2, x: 400 }}
+        //   drag
+      >
+        <div className="">
+          <Image src={profilePic} alt="me" width={200} height={200}></Image>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default FramerImgMe;
